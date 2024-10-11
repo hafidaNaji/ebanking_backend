884a6738fac7e1cc92f85e33814a27b151ac87e5
naji <hafidanaji13@gmail.com>
2024-10-11 22:07:25 +0100

    third commit:ebanking web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CustomersComponent } from './customers/customers.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { HttpClientModule } from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import { NewCustomerComponent } from './new-customer/new-customer.component';
+//import { EnvironmentsComponent } from './environments/environments.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CustomersComponent,
+    AccountsComponent,
+    NewCustomerComponent,
+   // EnvironmentsComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideHttpClient(withFetch()),
+    provideClientHydration(),
+
   ],
   bootstrap: [AppComponent]
 })
